327a4ebf6c1e506672d795eae2b022f60a5474a2
mahikam <76594479+mahikam@users.noreply.github.com>
2021-11-09 19:01:27 -0500

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,27 @@
+img="";
+var status="";
+function preload(){
+    img=loadImage("desk.jpg");
+}
+function setup(){
+    canvas= createCanvas(400,400);
+    canvas.position(480,225);
+    objectDetector= ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("desk_status").innerHTML="Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    }
+    else if(results){
+        console.log(results);
+    }
+}
+function draw(){
+    image(img,0,0,400,400);
+}
